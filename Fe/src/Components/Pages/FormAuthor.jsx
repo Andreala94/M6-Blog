013936b80b90d276c45a7bb4e2ed329d9d4c0f8a
Andrea Lauro <125922974+Andreala94@users.noreply.github.com>
2023-08-21 21:04:55 +0200

    commit

--- Fe/src/Components/Pages/FormAuthor.jsx
+++ Fe/src/Components/Pages/FormAuthor.jsx
@@ -1,71 +1,95 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Button, Form } from 'react-bootstrap';
 
 const FormAuthor = () => {
-    const name = useRef(null);
-    const surname = useRef(null);
-    const dob = useRef(null);
-    const email = useRef(null);
-    const password = useRef(null);
-    const avatar = useRef(null);
+   const [formData, setFormData] = useState({})
+   const [avatar, setAvatar] = useState(null)
 
-    const handleSubmit = async (event) => {
-        event.preventDefault(); // Evita l'invio del modulo predefinito
+  const onChangeHandleFile = (e) =>{
+    setAvatar(e.target.files[0])
+  }
 
-        const formData = new FormData();
-        formData.append('name', name.current.value);
-        formData.append('surname', surname.current.value);
-        formData.append('dob', dob.current.value);
-        formData.append('email', email.current.value);
-        formData.append('password', password.current.value);
-        formData.append('avatar', avatar.current.files[0]);
-        console.log(FormData);
+  console.log(formData);
 
+  const uploadAvatar = async (file)=>{
+    const avatarFile = new FormData()
+    avatarFile.append('avatar', file)
+    try {
+        const response = await fetch('http://localhost:5050/register/avatarCloud', {
+            method: "POST",
+            body: avatarFile
+        })
+            
+        return await response.json()
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault()
+    if(avatar ){
         try {
-            const response = await fetch('http://localhost:5050/register/authors', {
-                method: 'POST',
-               
-                body: formData,
-            });
-
-            if (response.ok) {
-                const jsonData = await response.json();
-                console.log(jsonData); // Logga la risposta JSON dal server
-            } else {
-                console.error('Errore nella risposta:', response.status);
+            const uploadAvatarImage = await uploadAvatar(avatar)
+            const formDataWithAvatar ={
+                ...formData,
+                avatar: uploadAvatarImage.avatar
             }
+            const response = await fetch('http://localhost:5050/register/authors', {
+                method: "POST",
+                body: JSON.stringify(formDataWithAvatar)
+            })
         } catch (error) {
-            console.error('Si è verificato un errore:', error);
+            console.log(error);
         }
-    };
-    const saveUser = () => {
-            localStorage.setItem('formData', JSON.stringify(FormData))
-         
-        }
-        
+    }
+    else{
+        console.log( "Perfavore seleziona almeno un immagine!")
+    }
+  }
+   
+        console.log(formData);
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} encType='multipart/form-data'>
             <Form.Group className="mb-3" controlId="formGroupName">
                 <Form.Label>NAME</Form.Label>
-                <Form.Control type="input" ref={name} placeholder="Name" />
+                <Form.Control type="input"  placeholder="Name" onChange={(e)=>setFormData({
+                    ...formData,
+                    name:e.target.value
+                })}/>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupSecondName">
-                <Form.Label>SECOND NAME</Form.Label>
-                <Form.Control type="input" ref={surname} placeholder="Second Name" />
+            <Form.Group className="mb-3" controlId="formGroupName">
+                <Form.Label>SURNAME</Form.Label>
+                <Form.Control type="input"  placeholder="Name" onChange={(e)=>setFormData({
+                    ...formData,
+                    surname:e.target.value
+                })}/>
+            
+            
+                
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupEmail">
                 <Form.Label>EMAIL ADDRESS</Form.Label>
-                <Form.Control type="email" ref={email} placeholder="Enter email" />
+                <Form.Control type="email"  placeholder="Enter email" onChange={(e)=>setFormData({
+                    ...formData,
+                    email:e.target.value
+                })} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupBirthDate">
                 <Form.Label>BIRTH DATE</Form.Label>
-                <Form.Control type="date" ref={dob} placeholder="Enter your birthdate" />
+                <Form.Control type="date"  placeholder="Enter your birthdate" onChange={(e)=>setFormData({
+                    ...formData,
+                    dob:e.target.value
+                })} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label>PASSWORD</Form.Label>
-                <Form.Control type="password" ref={password} placeholder="Password" />
+                <Form.Control type="password"  placeholder="Password" onChange={(e)=>setFormData({
+                    ...formData,
+                    password:e.target.value
+                })} />
                 <Form.Label>AVATAR</Form.Label>
-                <Form.Control type="file" ref={avatar} />
+                <Form.Control type="file" onChange={onChangeHandleFile} />
             </Form.Group>
 
             <Button variant="primary" type="submit">
@@ -76,134 +100,3 @@ const FormAuthor = () => {
 };
 
 export default FormAuthor;
-
-
-
-// const handleSubmit = async (event) => {
-//     event.preventDefault(); // Evita l'invio del modulo predefinito
-
-//     const formData = new FormData();
-//     formData.append('name', name.current.value);
-//     formData.append('secondName', secondName.current.value);
-//     formData.append('birthData', birthData.current.value);
-//     formData.append('email', email.current.value);
-//     formData.append('password', password.current.value);
-//     formData.append('avatar', avatar.current.files[0]);
-
-//     try {
-//         const response = await fetch('http://localhost:5050/register/authors', {
-//             method: 'POST',
-//             body: formData,
-//         });
-
-//         if (response.ok) {
-//             const jsonData = await response.json();
-//             console.log(jsonData); // Logga la risposta JSON dal server
-//         } else {
-//             console.error('Errore nella risposta:', response.status);
-//         }
-//     } catch (error) {
-//         console.error('Si è verificato un errore:', error);
-//     }
-// };
-
-
-// const FormAuthor = () => {
-//     const name = useRef(null)
-//     const secondName = useRef(null)
-//     const birthData = useRef(null)
-//     const email = useRef(null)
-//     const password = useRef(null)
-//     const avatar = useRef(null)
-
-//     const handleSubmit = () => {
-
-//         const data = {
-//             name: name.current.value,
-//             secondName: secondName.current.value,
-//             birthData: birthData.current.value,
-//             email: email.current.value,
-//             password: password.current.value,
-//             avatar: avatar.current.files[0],
-       
-//         }
-//         console.log(data);
-
-       
-        
-//         const FormData = async () => {
-//             try {
-//                 const data = await fetch('http://localhost:5050/register/authors',{
-//                     method: 'POST',
-//                     body: data, 
-//                 })
-
-//                 const jsonData = await data.json();
-
-//             } catch (error) {
-//                 console.error('Si è verificato un errore:', error);
-//             }
-        
-        
-//         }
-
-    
-//     }
-   
-
-
-
-// const saveUser = () => {
-//     localStorage.setItem('formData', JSON.stringify(FormData))
-// }
-// return (
-//     <Form onSubmit={handleSubmit}>
-//         <Form.Group className="mb-3" controlId="formGroupEmail">
-//             <Form.Label>NAME</Form.Label>
-//             <Form.Control type="input" ref={name} placeholder="Name" />
-//         </Form.Group>
-//         <Form.Group className="mb-3" controlId="formGroupPassword">
-//             <Form.Label>SECOND NAME</Form.Label>
-//             <Form.Control
-//                 type="input"
-//                 ref={secondName}
-//                 placeholder="Second Name"
-//             />
-//         </Form.Group>
-//         <Form.Group className="mb-3" controlId="formGroupEmail">
-//             <Form.Label>EMAIL ADDRESS</Form.Label>
-//             <Form.Control
-//                 type="email"
-//                 ref={email}
-//                 placeholder="Enter email"
-//             />
-//         </Form.Group>
-//         <Form.Group className="mb-3" controlId="formGroupEmail">
-//             <Form.Label>BIRTH DATE</Form.Label>
-//             <Form.Control
-//                 type="date"
-//                 ref={birthData}
-//                 placeholder="Enter your birthdate"
-//             />
-//         </Form.Group>
-//         <Form.Group className="mb-3" controlId="formGroupPassword">
-//             <Form.Label>PASSWORD</Form.Label>
-//             <Form.Control
-//                 type="password"
-//                 ref={password}
-//                 placeholder="Password"
-//             />
-//             <Form.Group className="mb-3" controlId="formGroupEmail">
-//                 <Form.Label>AVATAR</Form.Label>
-//                 <Form.Control type="file" ref={avatar} />
-//             </Form.Group>
-//         </Form.Group>
-
-//         <Button variant="primary" type='submit'>
-//             Submit
-//         </Button>
-//     </Form>
-// )
-
-
-// export default FormAuthor
